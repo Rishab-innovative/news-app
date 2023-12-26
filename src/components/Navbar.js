8a5686f069bfdfdf202a01807e18c2cc2d092100
Rishab-innovative <rishabkumardcool@gmail.com>
2023-12-26 23:48:05 +0530

    feat: adding business page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,12 @@ const Navbar = () => {
       <header className="text-gray-600 body-font px-6 ">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <div className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 gap-2">
-            <span className=" text-xl font-bold text-green-800">AjNews</span>
+            <Link to="/">
+              <span className=" text-xl font-bold text-green-800">
+                DailyNews
+              </span>
+            </Link>
+
             <li className="ml-5 text-slate-500 font-bold list-none">
               {date.toDateString()}
             </li>
--- src/pages/science.js
+++ src/pages/science.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import Layout from "../components/Layout";
-import { motion } from "framer-motion"
-import science from "../News/science.json"
-
+import { motion } from "framer-motion";
 
 const Science = () => {
   const [myData, setMyData] = useState([]);
@@ -15,55 +13,64 @@ const Science = () => {
   }, []);
   return (
     <>
-      <Layout>
-        <div className="md:text-4xl text-xl mt-[2rem] grid justify-center font-bold text-slate-500">
-          <h2>Top Headlines - science</h2>
+      {myData.length === 0 ? (
+        <div className="flex justify-center items-center h-screen">
+          <h1>Loading...</h1>
         </div>
+      ) : (
+        <Layout>
+          <div className="md:text-4xl text-xl mt-[2rem] grid justify-center font-bold text-slate-500">
+            <h2>Top Headlines - science</h2>
+          </div>
 
-        <section className="text-gray-600 body-font">
-  <div className="container px-5 py-12 mx-auto">
-    <div className="flex flex-wrap m-4">
-   
-        {
-            science.articles?.map((article)=>{
-                return(
-               
-                    
-                    (article.title && article.description && article.urlToImage)?
-                    
-                    <motion.div whileHover={{ scale: 1.1 }} className="p-4 md:w-1/3" key={article.title} >
-                    <div className="h-full overflow-hidden">
-                        <a href={article.url} target="_blank" rel="noreferrer" >
-                    <img className="lg:h-48 md:h-36 w-full object-cover object-center" src={article.urlToImage} alt="blog"/>
-                    </a>
-                    <div className="py-5">
-                      <a href={article.url} target="_blank" >
-                      <h2 className="title-font text-md font-medium text-slate-600 mb-2">{article.title.slice(0,40)}...</h2>
-                      </a>
+          <section className="text-gray-600 body-font">
+            <div className="container px-5 py-12 mx-auto">
+              <div className="flex flex-wrap m-4">
+                {myData?.map((article) => {
+                  return article.title &&
+                    article.description &&
+                    article.urlToImage ? (
+                    <motion.div
+                      whileHover={{ scale: 1.1 }}
+                      className="p-4 md:w-1/3"
+                      key={article.title}
+                    >
+                      <div className="h-full overflow-hidden">
+                        <a href={article.url} target="_blank" rel="noreferrer">
+                          <img
+                            className="lg:h-48 md:h-36 w-full object-cover object-center"
+                            src={article.urlToImage}
+                            alt="blog"
+                          />
+                        </a>
+                        <div className="py-5">
+                          <a href={article.url} target="_blank">
+                            <h2 className="title-font text-md font-medium text-slate-600 mb-2">
+                              {article.title.slice(0, 40)}...
+                            </h2>
+                          </a>
 
-                    <a href={article.url} target="_blank" rel="noreferrer" >
-                      <p className="leading-relaxed mb-3 text-sm">{article.description.slice(0,110)}...</p>
-                      </a>
-                   
-
-                     
-                    </div>
-                  </div></motion.div>:""
-                  
-                )
-            })
-        }
-
-
-
-       
-  
-     
-    </div>
-  </div>
-</section>
-
-      </Layout>
+                          <a
+                            href={article.url}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <p className="leading-relaxed mb-3 text-sm">
+                              {article.description.slice(0, 110)}...
+                            </p>
+                          </a>
+                        </div>
+                      </div>
+                    </motion.div>
+                  ) : (
+                    ""
+                  );
+                })}
+              </div>
+            </div>
+          </section>
+        </Layout>
+      )}
     </>
   );
 };
